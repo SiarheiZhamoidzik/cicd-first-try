f84da3b96c0a1ba86573f111fcd48f20ad864d2a
Siarhei Zhamoidzik <siarhei_zhamoidzik@epam.com>
2023-04-16 19:40:24 +0300

    test DB

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-if 'FOO' == 'foo'.upper():
-    print('Test Passed')
-else:
-    print('Test Failed')
+# if 'FOO' == 'foo'.upper():
+#     print('Test Passed')
+# else:
+#     print('Test Failed')
+    
+# DB CONNECTION pymssql
+
+pymssql_server = 'localhost'
+pymssql_user = 'auto_test'
+pymssql_password = 'auto_test'
+pymssql_database = 'AdventureWorks2019'
+    
+cnx = pymssql.connect(server=cnx_settings.pymssql_server, user=cnx_settings.pymssql_user,
+                      password=cnx_settings.pymssql_password, database=cnx_settings.pymssql_database)
